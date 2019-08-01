90dce6cf4ea0cc8adf9b053887c1529dbc74da7a
ModernScripting <52080861+ModernScripting@users.noreply.github.com>
2019-08-01 14:35:50 -0400

    Update Check version of remote installation.ps1

--- File Management/Check version of remote installation.ps1
+++ File Management/Check version of remote installation.ps1
@@ -1,1 +1,3 @@
 
+#Remotely check the version of a file. In this case Acrobat Reader DC
+Invoke-Command -ComputerName ExampleName -ScriptBlock {Get-Item -Path "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe").VersionInfo)
