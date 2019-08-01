b15e83662a78782da94d80fe05a3794c4e286e11
ModernScripting <52080861+ModernScripting@users.noreply.github.com>
2019-08-01 14:40:03 -0400

    Create Nessus Prescan.ps1

---
+++ Networking & Service Discovery/Nessus Prescan.ps1
@@ -0,0 +1,4 @@
+#Execute this script on the target standalone host to set the conditions for a Windows 10 Machine to get a full and complete Nessus scan
+
+#Start Windows services
+Start-Service RasAuto
