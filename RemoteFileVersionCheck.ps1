e5cea54ea3ebcd8d2cf11a7366ee32636a65817d
ModernScripting <52080861+ModernScripting@users.noreply.github.com>
2019-08-01 14:35:18 -0400

    Delete RemoteFileVersionCheck.ps1

--- RemoteFileVersionCheck.ps1
+++
@@ -1,2 +0,0 @@
-#Remotely check the version of a file. In this case Acrobat Reader DC
-Invoke-Command -ComputerName ExampleName -ScriptBlock {Get-Item -Path "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe").VersionInfo)
