b1c777bad24b91a7a524f40b7baaae0f4ab9a1a7
Joe Prichard <52080861+ModernScripting@users.noreply.github.com>
2019-08-14 13:15:29 -0400

    Update List users logged into a remote machine.ps1

--- User Management/List users logged into a remote machine.ps1
+++ User Management/List users logged into a remote machine.ps1
@@ -1,1 +1,5 @@
+#WMI Method. Dependent services - DCOM Server Process Launcher, RPC, RPC endpoint mapper, Windows Management Instrumentation
 Get-WMIObject -ComputerName ExampleHostName -Class Win32_ComputerSystem | Select-Object UserName
+
+$CIM method.
+Get-CIMInstance -ComputerName ExampleHostName -Class Win32_ComputerSystem | Select-Object UserName
