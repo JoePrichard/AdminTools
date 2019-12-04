2dccf45f270177c5e30cda0d68bbbb6b630efffa
JP <52080861+ModernScripting@users.noreply.github.com>
2019-12-04 08:40:48 -0500

    Update SCUpdates.ps1

--- HostManagement/SCUpdates.ps1
+++ HostManagement/SCUpdates.ps1
@@ -1,1 +1,16 @@
+#Force a host to install available updates from SCCM Software Center
 
+function Invoke-UpdateInstall
+{
+Param (
+  [String][Parameter(Mandatory=$True, Position=1)] $ComputerName,
+  [String][Parameter(Mandatory=$True, Position=2)] $UpdateName,
+  )
+Begin
+{
+  $AppEvalState = "0"
+  $AppEvalState = "1"
+  $ApplicationClass = [WmiClass]"root\ccm\clientSDK:CCM_SoftwareUpdatesManager"
+}
+
+Process
