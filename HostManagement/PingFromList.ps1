09e2a4fe5165a1513ac8abfb7cc47fbd5dba3d83
JP <52080861+ModernScripting@users.noreply.github.com>
2019-12-09 12:50:57 -0500

    Create PingFromList.ps1

---
+++ HostManagement/PingFromList.ps1
@@ -0,0 +1,4 @@
+#Script pulls a list of hosts from hosts.txt and uses Test-Connection function on each host. 
+#The result for each host is written to a timestamped text file with a count of online machines at the bottom
+#Another script will compare the text files to expose consistently offline machines
+#Could be tailored to run in certain time windows by scheduling when the script runs
