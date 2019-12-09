95fafab08eceffb67b8fe0fec6b5edaea16adba9
JP <52080861+ModernScripting@users.noreply.github.com>
2019-12-09 12:59:41 -0500

    Update PingFromList.ps1

--- HostManagement/PingFromList.ps1
+++ HostManagement/PingFromList.ps1
@@ -2,3 +2,18 @@
 #The result for each host is written to a timestamped text file with a count of online machines at the bottom
 #Another script will compare the text files to expose consistently offline machines
 #Could be tailored to run in certain time windows by scheduling when the script runs
+
+$hosts = Get-Content "C:\ModernScripting\hosts.txt"
+$hostdata = "C:\ModernScripting\hostdata $(Get-Date -Format yyyy-MM-dd).txt"
+$totalhostsup = 0
+
+foreach ($x in $hosts) {
+  if (Test-Connection -ComputerName $x -Count 1 -ErrorAction SilentlyContinue) {
+    Write-Host "$x, Online"
+}
+  else {
+    Write-Host "$x, Offline"
+    Add-Content -Path $hostdata -Value "x, Offline"
+    }
+}
+Add-Content -Path $hostdata -Value "Total Hosts Up: $totalhostsup"
